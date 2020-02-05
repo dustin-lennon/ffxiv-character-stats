b6da66f3cb55add0c5a4f3d7eeb35cd23defc837
Dustin Lennon <demonicpagan@gmail.com>
2020-02-04 22:47:28 -0500

    Renamed file and other minor changes

--- ffxiv-character-stats.php
+++ ffxiv-character-stats.php
@@ -3,31 +3,14 @@
  * Plugin Name:     Final Fantasy XIV Character Statistics
  * Plugin URI:      https://ffxiv.stelth2000inc.com
  * Description:     Pull Final Fantasy XIV character data using XIVAPI.
- * Version:         1.0
- * Requires PHP:    7.3
  * Author:          Dustin Lennon (Demonic Pagan on Sargatanas, Aether Data Center)
+ * Version:         1.0
  * Author URI:      https://ffxiv.stelth2000inc.com
- * License:         GPL v2 or later
- * License URI:     http://www.gnu.org/licenses/gpl-2.0.txt
- *
- * Copyright 2020 Dustin Lennon (email: dustin.lennon@stelth2000inc.com)
- * Final Fantasy XIV Character Statistics is free software: you can redistribute
- * it and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the License, or
- * any later version.
- *
- * Final Fantasy XIV Character Statistics is distributed in the hope that it will
- * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
- * License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * (Final Fantasy XIV Character Statistics). If not, see
- * (http://www.gnu.org/licenses/gpl-2.0.txt).
+ * Network:         false
  */
 
 // Make sure plugin can only be installed via admin interface
-if (defined('WP_INSTALLING') && WP_INSTALLING)  {
+if (defined('WP_INSTALLING') && WP_INSTALLING) {
     return;
 }
 
@@ -51,4 +34,12 @@ foreach ($wp_plugin_paths as $dir => $realdir) {
 if (!defined('FFXIV_PLUGIN_FCPATH')) {
     define('FFXIV_PLUGIN_FCPATH', __FILE__);
     define('FFXIV_PLUGIN_PATH', trailingslashit(dirname(FFXIV_PLUGIN_FCPATH)));
+}
+
+if (get_option('ffxivPluginActivated') != 1) {
+    add_action('activated_plugin', 'ffxivPlugin_save_activation_error');
+
+    function ffxivPlugin_save_activation_error() {
+        update_option('ffxivPlugin_act_error', ob_get_contents());
+    }
 }
